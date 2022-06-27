9e654316ebb2d7eb79e3180f5229375e6135307b
Richard <richard031104@outlook.com>
2022-06-26 22:19:54 -0400

    Buttons Finished

--- script.js
+++ script.js
@@ -1,9 +1,64 @@
 const sketchArea = document.querySelector("#sketchArea");
 const gridItem = document.querySelectorAll(".grid-item");
-let color = document.querySelector("#colorWheel").value;
+const colorWheel = document.querySelector("#colorWheel");
+
+const colorMode = document.querySelector("#colorMode");
+const rainbowMode = document.querySelector("#rainbowMode");
+const eraser = document.querySelector("#eraserMode");
+const clear = document.querySelector("#clear");
+const toggleGrid = document.querySelector("#toggleGrid");
+const gridSlider = document.querySelector("#gridSlider");
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+let currentMode = "colorMode";
+function changeMode(newMode) {
+  activeButton(newMode);
+  currentMode = newMode;
+}
+
+function activeButton(mode) {
+  const newButton = document.querySelector(`#${mode}`);
+  const buttons = document.querySelectorAll(
+    "button:not(#clear):not(#toggleGrid)"
+  );
+  buttons.forEach((button) => {
+    button.classList.remove("active");
+  });
+  newButton.classList.add("active");
+}
+
+colorWheel.oninput = () => {
+  colorMode.setAttribute("style", `background-color: ${colorWheel.value};`);
+};
+colorMode.onclick = () => changeMode("colorMode");
+rainbowMode.onclick = () => changeMode("rainbowMode");
+eraser.onclick = () => changeMode("eraserMode");
+clear.onclick = () => clearGrid();
+gridSlider.onchange = (e) => changeGrid(e.target.value);
+
+const changeColor = (e) => {
+  if (e.type === "mouseover" && !mouseDown) return;
+  let color = "";
+  if (currentMode == "colorMode") {
+    color = document.querySelector("#colorWheel").value;
+  } else if (currentMode == "rainbowMode") {
+    const radomColor = Math.floor(Math.random() * 16777215).toString(16);
+    color = `#${radomColor}`;
+  } else if (currentMode == "eraserMode") {
+    color = "#ffffff";
+  }
+  e.target.setAttribute("style", `background-color: ${color};`);
+  console.log(color);
+};
 
 gridItem.forEach((item) => {
-  item.addEventListener("click", () => {
-    item.setAttribute("style", "#3f3f3f");
-  });
+  item.addEventListener("mouseover", changeColor);
+  item.addEventListener("mousedown", changeColor);
 });
+
+const changeGrid = (value) => {
+  sketchArea.innerHTML = "";
+};
